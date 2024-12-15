6068061733a00f23b8b37b094b484d307d433727
LyubovSL <lyubov.sliva1@gmail.com>
2024-12-15 16:23:36 +0200

    my exception handling homework

--- homework1.1.py
+++ homework1.1.py
@@ -1,9 +1,15 @@
 import datetime
 
 def get_days_from_today(date):
-    current_date = datetime.datetime.strptime(date, "%Y-%m-%d")
-    today_date = datetime.datetime.today()
-    days_difference = today_date.toordinal() - current_date.toordinal()
-    return days_difference
+    try:
+        current_date = datetime.datetime.strptime(date, "%Y-%m-%d")
+        today_date = datetime.datetime.today()
+        days_difference = today_date.toordinal() - current_date.toordinal()
+        return days_difference
+    except ValueError:
+        return "Invalid date format. Use YYYY-MM-DD."
+    except TypeError:
+        return "Provided date is of an invalid type."
+
 
 print (get_days_from_today("2021-10-09"))
--- homework1.2.py
+++ homework1.2.py
@@ -1,10 +1,12 @@
 from random import randint
 def get_numbers_ticket(min, max, quantity):
-
-    result_array = set()
-    while len(result_array) != quantity:
-        result_array.add(randint(min, max))
-    numbers = sorted(list(result_array))
-    return numbers
-lottery_numbers = get_numbers_ticket(1,36, 5)
+    if min < 1 or max > 1000 or quantity < 1 or quantity > (max - min + 1):
+        return "Введіть число від 1 до 1000"
+    else:
+        result_array = set()
+        while len(result_array) != quantity:
+            result_array.add(randint(min, max))
+        numbers = sorted(list(result_array))
+        return numbers
+lottery_numbers = get_numbers_ticket(1,1036, 5)
 print("Ваші лотерейні числа:", lottery_numbers)
