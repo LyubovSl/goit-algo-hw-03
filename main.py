a518c97b82618974b4a6a17c9d08688ad8354dbf
LyubovSl <lyubov.sliva1@gmail.com>
2024-12-15 16:45:44 +0200

    Delete main.py

--- main.py
+++
@@ -1,5 +0,0 @@
-one_line_text = "Textual data in Python is handled with str objects," \
-                " or strings. Strings are immutable sequences of Unicode" \
-                " code points. String literals are written in a variety " \
-                " of ways: single quotes, double quotes, triple quoted."
-print(one_line_text)
